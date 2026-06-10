4dfe9e4f4bf962f4eddd5bd7f56dcb99018d007b
Christian Mattes <CarpeNecopinum@users.noreply.github.com>
2026-06-10 23:13:18 +0200

    Publish the observed output power sent to the inverter

--- handlers/dc_message.go
+++ handlers/dc_message.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"log"
 	"path"
+	"strconv"
 
 	"github.com/dsnet/try"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -15,17 +16,18 @@ type DCMessageHandler struct {
 }
 
 type DCMessage struct {
-	Ka0 int16
-	Ka1 int16
-	Ka2 int16
-	Ka3 int16
-	Ka4 int16
-	Ka5 int16
-	Ka6 int8
+	Ka0       int8
+	Ka1       int16
+	Ka2       int16
+	OutPower1 int16
+	Ka3       int16
+	Ka4       int16
+	OutPower2 int16
 }
 
 func (s *DCMessageHandler) RegisterOn(c mqtt.Client) {
 	listen_topic := path.Join(InputPrefix, s.DeviceId, "33", "state")
+	output_topic := path.Join(OutputPrefix, s.DeviceId, "sent_power", "state")
 
 	tok := c.Subscribe(listen_topic, 1, func(c mqtt.Client, m mqtt.Message) {
 		defer try.F(log.Println)
@@ -35,6 +37,10 @@ func (s *DCMessageHandler) RegisterOn(c mqtt.Client) {
 		try.E(binary.Read(r, binary.BigEndian, &data))
 
 		log.Printf("Read DCMessage: %+v\n", data)
+
+		combined := int(data.OutPower1) + int(data.OutPower2)
+
+		c.Publish(output_topic, 1, false, strconv.Itoa(combined))
 	})
 	tok.Wait()
 	if tok.Error() != nil {
